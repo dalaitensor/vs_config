1bd40ba8351eb4767334f0e686861fa66c390da8
dalaicern <luciferb85@gmail.com>
2024-12-20 11:54:56 +0800

    asdcds

--- a.cpp
+++ a.cpp
@@ -8,11 +8,15 @@
 #define debug(x) cout << #x << " = " << x << " "
 #define debug2(x, y) cout << #x << " = " << x << " " << #y << " = " << y << " "
 #define debug3(x, y, z) cout << #x << " = " << x << " " << #y << " = " << y << " " << #z << " = " << z << " "
-const int MOD = int(1e9) + 7;
+
 
 using namespace std;
 
+typedef struct{
+   int x , k; 
+} prime;
 
+const int MOD = int(1e9) + 7;
 
 class Sol {
 private:
@@ -21,28 +25,50 @@ private:
     
  
 public:
+    ll power(ll base, ll exp, ll m){
+        if(exp == 0) return 1;
+        if(exp == 1) return base % m;
+        ll temp = power(base, exp / 2, m);
+        temp = (temp * temp) % m;
 
-    void helper(int x, bool up , int par, string s){
-        if(x == 1){
-            s += par + '0';
-            cout << s << endl;
-            return;
-        } 
-        s += par + '0';
-        int a =  up ? 0 : 1;
-        helper(x - 1, true, a , s);
-        helper(x - 1, false, 1 - a , s);
+        if(exp & 1){
+            temp = (temp * base) % m;
+        }
+
+        return temp;
     }
 
 
     void solve() {
         cin >> n;
+        vector<prime> arr(n);
 
-        string res;
-        helper(n, true, 0, res);
-        helper(n, false, 1, res);
+        ll number = 0;
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i].x >> arr[i].k;
+            number += arr[i].k;
+        } 
 
+        debug(number) << endl;
 
+        ll fact = 1;
+
+        for(int i = 1; i <= number; i++){
+            fact = (fact * i) % MOD;
+        }
+
+        debug(fact) << endl;
+
+        ll sum = 0;
+        ll prod = 1ll;
+
+        for(int i = 0; i < n; i++) {
+            sum += (arr[i].x * arr[i].k) % MOD;
+            prod *= power(arr[i].x, arr[i].k, MOD);
+        }
+
+        cout << fact << " " << sum << " " << prod << endl;
+         
     }
 };
 
@@ -56,7 +82,7 @@ freopen("output.txt","w",stdout);
     ios::sync_with_stdio(0);
     cin.tie(0), cout.tie(0);
     int t = 1;
-    // cin >> t;
+    // cin >> t; 
     while(t--) {
         Sol s;
         s.solve();
--- b.cpp
+++ b.cpp
@@ -2,6 +2,8 @@
 
 #define ll              long long
 #define vi              vector<int >
+#define vil             vector<ll >
+#define pb              push_back
 #define pii             pair<int , int >
 #define R(i , n, a) for(int i = n; i >= a; i--)
 #define debug(x) cout << #x << " = " << x << " "
@@ -16,7 +18,16 @@ private:
 
 public:
     void solve() {
-        cin >> n;
+        // cin >> n;
+        printf("%d asdcsa", n);
+        // vi pref(n+1), dp(n + 1);
+
+        // for (int i = 1; i <= n; i++) {
+        //     dp[i] = 1 + pref[i - 1];
+        //     pref[i] = dp[i] + pref[i - 1];
+        // }
+
+        // cout << dp[n];
     }
 };
 
@@ -30,7 +41,7 @@ freopen("output.txt","w",stdout);
     ios::sync_with_stdio(0);
     cin.tie(0), cout.tie(0);
     int t = 1;
-    cin >> t;
+    // cin >> t;
     while(t--) {
         Sol s;
         s.solve();
