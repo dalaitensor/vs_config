1df532459174f6b2b123c0d728057614a83b3d59
dalaicern <luciferb85@gmail.com>
2025-01-17 22:19:10 +0800

    adscs

---
+++ A/sol.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+//#define int              long long
+#define vi              vector<int >
+#define vii              vector<vector<int > >
+#define pii             pair<int , int >
+#define R(i , n, a) for(int i = n; i >= a; i--)
+#define all(x) begin(x), end(x)
+#define debug(x) cout << #x << " = " << x << " "
+#define debug2(x, y) cout << #x << " = " << x << " " << #y << " = " << y << " "
+#define debug3(x, y, z) cout << #x << " = " << x << " " << #y << " = " << y << " " << #z << " = " << z << " "
+
+using namespace std;
+const int MOD = 1e9 + 7;
+
+void solve(){
+    
+}
+
+
+signed main() {
+
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+
+}
