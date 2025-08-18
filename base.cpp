60fc673483815c7a19765938ae4861db18ad17f3
dalaitseren <77785874+dalaitensor@users.noreply.github.com>
2025-08-18 22:36:47 +0800

    Create base.cpp

---
+++ base.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int             long long
+#define pii             pair<int, int>
+#define FOR(i,a ,b)      for(int i=a; i < b; i++)
+#define ROF(i, a, b)      for(int i = a; i >= b; i--)
+#define all(x)              x.begin(), x.end()
+template<typename T, typename U>
+ostream& operator<<(ostream &os, pair<T, U> p)
+{
+    return os << "(" << p.first << "," << p.second << ")";
+}
+template<typename T>
+void print(T v, int lim = 1e9)
+{
+    for(auto x : v)
+        if(lim-- > 0) cout << x << " ";
+    cout << endl;
+}
+signed main()
+{
+    int test = 1;
+    cin >> test;
+    while(test--){
+
+    }
+}
